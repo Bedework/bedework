eb02d0777dfe6a602a3899bbdff140049a85ada8
mike <mikeadouglass@gmail.com>
2018-05-18 17:03:20 -0400

    Add script to stoph2 and update starth2 to make it possible.

--- build/quickstart/linux/starth2.sh
+++ build/quickstart/linux/starth2.sh
@@ -20,8 +20,22 @@ JBOSS_CONFIG="standalone"
 JBOSS_SERVER_DIR="$BASE_DIR/$JBOSS_VERSION/$JBOSS_CONFIG"
 JBOSS_DATA_DIR="$JBOSS_SERVER_DIR/data"
 
+TMP_DIR="$JBOSS_SERVER_DIR/tmp"
+PIDFILE="$TMP_DIR/h2.pid"
+
+if [ ! -d "$TMP_DIR" ]; then
+  mkdir -p $TMP_DIR
+fi
+
+if [ -f "$PIDFILE" ]; then
+  printf "Warning: pidfile $PIDFILE exists - trying to shut down running process"
+  $BASE_DIR/stoph2
+fi
+
+rm $PIDFILE
+
 BW_DATA_DIR=$JBOSS_DATA_DIR/bedework
 
 H2cp="$BASE_DIR/$JBOSS_VERSION/modules/system/layers/base/com/h2database/h2/main/h2*.jar"
 
-$JAVA -cp $H2cp org.h2.tools.Server -tcp -web -baseDir $BW_DATA_DIR/h2 &
+$JAVA -cp $H2cp org.h2.tools.Server -tcp -web -baseDir $BW_DATA_DIR/h2 & echo $! >>$PIDFILE
---
+++ build/quickstart/linux/stoph2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BASE_DIR=`pwd`
+JBOSS_VERSION="wildfly-10.1.0.Final"
+
+JBOSS_CONFIG="standalone"
+JBOSS_SERVER_DIR="$BASE_DIR/$JBOSS_VERSION/$JBOSS_CONFIG"
+
+TMP_DIR="$JBOSS_SERVER_DIR/tmp"
+PIDFILE="$TMP_DIR/h2.pid"
+
+if [ -e $PIDFILE ]; then
+  printf "Shutting down h2:  "
+  kill -15 `cat $PIDFILE`
+  rm $PIDFILE
+else
+  echo "h2 doesn't appear to be running."
+fi
