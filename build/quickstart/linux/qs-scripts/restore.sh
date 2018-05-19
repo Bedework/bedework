a8ab6c31672ca5cf60c59971885e98f95d26a104
mike <mikeadouglass@gmail.com>
2018-05-19 00:11:07 -0400

    Restore scripts and data

---
+++ build/quickstart/linux/qs-scripts/restore.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# This must be executed from the quickstart home directory.
+
+BASE_DIR=`pwd`
+
+# If you want them somewhere else then symlink. and symlink to this script to make it easier
+
+JBOSS_VERSION="wildfly-10.1.0.Final"
+
+if [ ! -d "$JBOSS_VERSION" ]; then
+  echo "Not located in the quickstart"
+  exit 1
+fi
+
+resources=$BASE_DIR/bedework/build/quickstart
+
+JBOSS_CONFIG="standalone"
+JBOSS_SERVER_DIR="$BASE_DIR/$quickstart/$JBOSS_VERSION/$JBOSS_CONFIG"
+JBOSS_DATA_DIR="$JBOSS_SERVER_DIR/data"
+h2_data_dir="$JBOSS_DATA_DIR/bedework/h2"
+es_data_dir="$JBOSS_DATA_DIR/bedework/elasticsearch"
+
+TMP_DIR="$JBOSS_SERVER_DIR/tmp"
+
+echo "Temp dir is $TMP_DIR"
+
+if [ ! -d "$TMP_DIR" ]; then
+  mkdir -p $TMP_DIR
+fi
+
+# This script will do the following
+#     stop h2
+#     replace the h2 database files
+#     start h2
+#     replace the elasticsearch data
+#     stop the directory
+#     Replace the directory data
+#     start the directory
+#
+
+cd $BASE_DIR
+./stoph2
+
+cd $TMP_DIR/
+
+rm -f h2.zip
+
+cp $resources/data/h2.zip $TMP_DIR
+
+rm -rf h2/
+
+unzip h2.zip
+
+rm -f h2.zip
+
+cp -r h2 $h2_data_dir/
+rm -rf h2/
+
+cd $BASE_DIR
+./starth2
+
+# ------------------------------------- ES data
+
+cd $TMP_DIR/
+rm elasticsearch.zip
+rm -rf elasticsearch
+cp $resources/data/elasticsearch.zip .
+
+unzip elasticsearch.zip
+
+rm elasticsearch.zip
+
+rm -rf $es_data_dir/elasticsearch
+cp -r elasticsearch $es_data_dir/
+
+# ------------------------------------- directory data
+
+cd $BASE_DIR
+./dirstop
+
+rm -rf apacheds-1.5.3-fixed
+cp $resources/data/apacheds.zip .
+unzip apacheds.zip
+rm apacheds.zip
+
+./dirstart
