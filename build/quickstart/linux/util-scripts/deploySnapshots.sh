d724b64e6267a462211de1617cbb39769d2269bc
mike <mikeadouglass@gmail.com>
2025-02-08 22:29:57 -0500

    All modules now build in the jakarta world.

--- build/quickstart/linux/util-scripts/deploySnapshots.sh
+++ build/quickstart/linux/util-scripts/deploySnapshots.sh
@@ -51,14 +51,14 @@ deploySnapshot bw-category
 deploySnapshot bw-calendar-common
 deploySnapshot bw-calendar-engine
 deploySnapshot bw-calendar-dumprestore
-
-
 deploySnapshot bw-calendar-client
 deploySnapshot bw-calendar-xsl
 deploySnapshot bw-calendar-deploy
 deploySnapshot bw-testsuite
 deploySnapshot bw-wf-extensions
 deploySnapshot bw-quickstart
+
+
 deploySnapshot bw-wfmodules
 deploySnapshot bw-wildfly-galleon-feature-packs
 deploySnapshot bedework
